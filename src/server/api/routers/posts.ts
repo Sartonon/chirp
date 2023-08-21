a4780597e4485cdccb27436b9b465abf58d34e10
Santeri Rusila <santtu_1993@hotmail.com>
2023-08-21 11:28:32 +0300

    some changes

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,7 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
+  getAll: publicProcedure.query(async ({ ctx }) => {
     return ctx.prisma.post.findMany();
   }),
 });
